27e6183d5e2a14e5d5a7ec57b4f020d9926991ba
Danilo Sanches <silva.danilosanches@gmail.com>
2020-09-05 18:12:01 -0300

    Adicionado vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">bezKoder</a>
+        <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="/tutoriais" class="nav-link">Tutoriais</a>
+            </li>
+            <li class="nav-item">
+                <a href="/add" class="nav-link">Novo</a>
+            </li>
+        </div>
+    </nav>
+
+    <div class="container mt-3">
+        <router-view />
+    </div>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default {
+    name: 'app'
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    alias: '/tutoriais',
+    name: 'TutorialList',
+    component: () => import("../views/TutorialList.vue")
+  },
+  {
+    path: '/tutoriais/:id',
+    name: 'Tutorial',
+    component: () => import('../views/Tutorial.vue')
+  },
+  {
+    path: '/tutoriais/add',
+    name: 'Novo',
+    component: () => import('../views/AddTutorial.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
